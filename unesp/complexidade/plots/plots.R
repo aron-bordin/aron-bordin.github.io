754e5c1e90e54d3e900f46b9e7d639bd533a39fd
Aron Bordin <aron.bordin@gmail.com>
2015-11-17 09:24:21 -0200

    fixes

--- unesp/complexidade/plots/plots.R
+++ unesp/complexidade/plots/plots.R
@@ -1,8 +1,9 @@
-library(ggplot2)
-
 grafico1 <- function(n){
 	eq1 <- function(x){0.5*x*x}
+	data1 <- do.call(rbind, lapply(1:n, eq1))
 	eq2 <- function(x){6*x*log2(x)+6*x}
-	p <- ggplot(data.frame(x = c(0,n)), aes(x))
-	p + stat_function(fun = eq1, colour="red") + stat_function(fun=eq2,colour="blue")	
+	data2 <- do.call(rbind, lapply(1:n, eq2))
+	
+	plot(1:n,data1,type="l",col="red", xlab="N", ylab="Tempo de execução")	
+	lines(1:n,data2,col="blue")
 }
